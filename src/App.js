5aaa26e898e273742b2b4e3178afdec8d1c7cdd7
learntopilk <jonsterivilja@gmail.com>
2018-06-06 18:56:43 +0300

    Tehtävä 5.1 tehty
    Sisäänkirjaus onnistuu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import Blog from './components/Blog'
 import Login from './components/Login'
+import loginService from './services/login'
 import blogService from './services/blogs'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      newBlog: '',
+      username: '',
+      password: '',
+      token: null,
+      error: '',
+      user: null
+
     }
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    console.log('logging in')
+
+    try {
+      const userObj = { username: this.state.username, password: this.state.password }
+      const user = await loginService.login(userObj)
+
+      this.setState({ username: '', password: '', user })
+
+    } catch (err) {
+      console.log(err)
+      this.setState({ error: 'Käyttäjätunnus tahi salasana onpi virheellinen' })
+      setTimeout(() => { this.setState({ error: '' }) }, 5000)
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    event.preventDefault()
+    console.log("1 ", event.target.value, " ", event.target.name)
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   componentDidMount() {
@@ -18,9 +49,12 @@ class App extends React.Component {
   }
 
   render() {
-    if (!this.state.token) {
+    if (this.state.user === null) {
       return (
-        <Login />
+        <div>
+          <h3 class="error">{this.state.error}</h3>
+          <Login login={this.login} handleLoginFieldChange={this.handleLoginFieldChange} state={this.state} />
+        </div>
       )
     } else {
       return (
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Login = (props) => {
+const Login = ({state, handleLoginFieldChange, login}) => {
     return (
         <div>
             <h4>Please log in</h4>
-            <form action="/api/login">
-                <div><span>username: <input type="text" name="username" /></span></div>
-                <div><span>password: <input type="password" name="password" /></span></div>
+            <form onSubmit={login}>
+                <div><span>username: <input type="text" name="username" onChange={handleLoginFieldChange} value={state.username} /></span></div>
+                <div><span>password: <input type="password" name="password" onChange={handleLoginFieldChange} value={state.password} /></span></div>
+                <button type="submit">login</button>
             </form>
 
         </div>
