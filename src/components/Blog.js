273c77a058fa5091c552948c09a26e71d8d62c78
learntopilk <jonsterivilja@gmail.com>
2018-06-07 19:42:37 +0300

    Tehtävä 5.6 tehty
    Toisin kuin edellisessä mainittu, 5.6 on nyt tehty - blogin tarkemmat tiedot näkyvät otsikkoa klikattaessa.

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,57 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false,
+      blog: props.blog,
+      title: props.title,
+      author: props.author,
+      url: props.url
+
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  handleClick = (e) => {
+    e.preventDefault()
+    this.setState
+  }
+
+
+
+  render() {
+
+    const stil = {
+      'paddingLeft': '10px',
+      border: '2px solid black',
+      'borderRadius': '4px',
+      margin: '5px'
+    }
+
+    //console.log("blog: ", this.state.blog)
+    const hideWhenVisible = { display: this.state.visible ? 'none' : '' }
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+    return (
+      <div style={stil}>
+
+        <h5 onClick={this.toggleVisibility}>{this.state.blog.title}</h5>
+        <p style={showWhenVisible}>Author: {this.state.blog.author}</p>
+        <p style={showWhenVisible}>Address: {this.state.blog.url}</p>
+        <div style={showWhenVisible}><span>likes: {this.state.blog.likes}</span><button onClick={this.handleClick}>like</button></div>
+
+
+      </div>
+
+
+    )
+  }
+
+
+}
 
 export default Blog
