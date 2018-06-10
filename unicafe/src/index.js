55444787e37e72c89f49e76c1cf0d2790de806c2
learntopilk <jonsterivilja@gmail.com>
2018-06-10 21:08:59 +0300

    Väli-commit

--- unicafe/src/index.js
+++ unicafe/src/index.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+
+const feedbackReducer = (state = { pos: 0, neutr: 0, neg: 0 }, action) => {
+  let pos = state.pos
+  let neutr = state.neutr
+  let neg = state.neg
+
+  switch (action.type) {
+    case 'POSITIVE':
+      pos = pos + 1
+      return ({ pos, neutr, neg })
+    case 'NEUTRAL':
+      neutr = neutr + 1
+      return ({ pos, neutr, neg })
+    case 'NEGATIVE':
+      neg = neg + 1
+      return ({ pos, neutr, neg })
+  }
+}
+
 
 const Statistiikka = () => {
   const palautteita = 0
